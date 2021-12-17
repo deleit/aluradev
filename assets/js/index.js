790c16722334167b658a45908974f3d608f3de1b
Deivison Leite <dlmacedo9@gmail.com>
2021-12-17 10:00:18 -0300

    Implementação do destaque de sintaxe

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,24 @@
 const colorSelected = document.getElementById('bg-color-selector');
 const backgroundColor = document.getElementById('bg-color');
 
-colorSelected.addEventListener('change', (event) => {
+colorSelected.addEventListener('input', (event) => {
     backgroundColor.style.backgroundColor = event.target.value;
+});
+
+// highlight.js
+const highlightButton = document.querySelector('#highlight-btn');
+const codeArea = document.querySelector('.code-editor__wrapper');
+const language = document.querySelector('#languages');
+
+const applyHighlight = () => {
+    const code = codeArea.innerText;
+    console.log(code)
+    codeArea.innerHTML = `<code class="code-editor__textarea hljs ${language.value}" id="code-editor" spellcheck="false" contenteditable="true"></code>`;
+    codeArea.querySelector('code').textContent = code;
+    console.log(codeArea.querySelector('code'))
+    hljs.highlightElement(codeArea.querySelector('code'));
+}
+
+highlightButton.addEventListener('click', () => {
+    applyHighlight();
 });
